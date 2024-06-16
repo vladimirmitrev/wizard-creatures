382370d669db6d683743a937d4c975e6bb30f8d1
Vladimir Mitrev <orionsbelt1017@gmail.com>
2024-06-16 11:04:16 +0300

    Add auth middleware, add auth service

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,40 @@
+const jwt = require('../lib/jsonwebtoken');
+const { SECRET } = require('../config');
+
+exports.authMiddleware = async (req, res, next) => {
+    const token = req.cookies['auth'];
+
+    if (!token) {
+        return next();
+    }
+
+    try {
+        const decodedToken = await jwt.verify(token, SECRET);
+
+        req.user = decodedToken;
+        res.locals.isAuthenticated = true;
+        res.locals.user = decodedToken;
+        
+        next();
+    } catch (err) {
+        // console.log(err);
+        res.clearCookie('auth');
+        res.redirect('/auth/login');
+    }
+}
+
+exports.isAuth = (req, res, next) => {
+    if (!req.user) {
+        return res.redirect('/auth/login');
+    }
+
+    next();
+}
+
+exports.isGuest = (req, res, next) => {
+    if (req.user) {
+        return res.redirect('/');
+    }
+
+    next();
+}
---
+++ services/authService.js
@@ -0,0 +1,48 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('../lib/jsonwebtoken');
+const { SECRET } = require('../config');
+
+exports.register = async (userData) => {
+  if (userData.password !== userData.rePassword) {
+    throw new Error('Password mismatch!');
+  }
+
+  const user = await User.findOne({ email: userData.email });
+  if (user) {
+    throw new Error('Email already taken!');
+  }
+
+  const createdUser = await User.create(userData);
+
+  const token = await generateToken(createdUser);
+
+  return token;
+};
+
+exports.login = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error('Invalid email or password!');
+  }
+
+  const isValid = await bcrypt.compare(password, user.password);
+  if (!isValid) {
+    throw new Error('Invalid email or password!');
+  }
+
+  const token = await generateToken(user);
+
+  return token;
+};
+
+function generateToken(user) {
+  const payload = {
+    _id: user._id,
+    username: user.username,
+    email: user.email,
+  }
+
+  return jwt.sign(payload, SECRET, {expiresIn: '1d'});
+}

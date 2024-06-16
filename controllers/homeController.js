83644f74297cdade6d30709a1d40cd6379439bc1
Vladimir Mitrev <orionsbelt1017@gmail.com>
2024-06-16 14:42:44 +0300

    Add my-post page

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,10 +1,19 @@
 const router = require('express').Router();
 const { getErrorMessage } = require('../utils/errorUtils');
 const { isAuth } = require('../middlewares/authMiddleware');
+const userService = require('../services/userService');
 
 router.get('/', async (req, res) => {
 
   res.render('home');
+});
+
+router.get('/my-posts', isAuth, async (req, res) => {
+  const user = await userService.getInfo(req.user._id).lean();
+  // const userFirstName = user.firstName;
+  // const userLastName = user.lastName;
+   
+  res.render('my-posts', { user });
 });
 
 module.exports = router;
